245315898daa153a9e58836858b06349a3eb280d
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2023-10-13 09:36:25 +0300

    money_box2

--- куліковський-георгій/moneyBox/main.py
+++ куліковський-георгій/moneyBox/main.py
@@ -2,13 +2,10 @@ from money_box import Money_Box
 
 
 def main():
-    money_box = Money_Box(400, 200)
-    money_box.add_coin()
+    money_box = Money_Box(400)
+    money_box.add_coin(100)
     print(money_box)
-    money_box.add_amount(100)
-    print(money_box)
-    print(money_box.can_add(2000))
-    money_box.add_amount(2000)
-    
+    money_box.add_coin(500)
+
 
 main()
--- куліковський-георгій/moneyBox/money_box.py
+++ куліковський-георгій/moneyBox/money_box.py
@@ -1,23 +1,12 @@
 class Money_Box:
     
     
-    def __init__(self, max_amount, goal):
-        self.max_amount = max_amount
-        self.goal = goal
+    def __init__(self, capacity):
+        self.capacity = capacity
         self.current_amount = 0
         
-        
-    def add_coin(self):
-        if self.goal - 1 == self.current_amount:
-            self.current_amount += 1
-            print("Goal is achieved")
-        elif self.goal == self.current_amount:
-            print("Goal is already achieved")
-        else:
-            self.current_amount += 1
-            
     
-    def add_amount(self, amount):
+    def add_coin(self, amount):
         if self.can_add(amount):
             self.current_amount += amount
         else:
@@ -25,11 +14,11 @@ class Money_Box:
             
     
     def can_add(self, amount):
-        if self.goal - (self.current_amount + amount) >= 0:
+        if self.capacity - (self.current_amount + amount) >= 0:
             return 1
         else:
             return 0
         
         
     def __str__(self):
-        return f'Goal:{self.goal}, Max amount:{self.max_amount}, Current amount:{self.current_amount}'
+        return f'Max amount:{self.capacity}, Current amount:{self.current_amount}'
