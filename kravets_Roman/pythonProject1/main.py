b793a84e5c592310b19197e519a75bef761913e4
PAnda2029 <132852660+PAnda2029@users.noreply.github.com>
2023-10-13 11:00:10 +0300

    Add files via upload

--- kravets_Roman/pythonProject1/main.py
+++ kravets_Roman/pythonProject1/main.py
@@ -1,46 +1,49 @@
-def retrieve_vector(file_name: str) -> list[int]:
-    try:
-        with open(file_name, 'r') as file:
-            vector = [int(value) for value in file.read().split()]
-        return vector
-    except FileNotFoundError:
-        print(f"File '{file_name}' not found.")
-        return []
-    except ValueError:
-        print(f"Invalid data in file '{file_name}'. Please make sure the file contains only integers.")
-        return []
+class MoneyBox:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.coins = 0
 
+    def can_add(self, v):
+        return self.coins + v <= self.capacity
 
-def plus(first: list[int], second: list[int]) -> list[int]:
-    return [int(round(first[i] + second[i])) for i in range(len(first))]
-
-
-def minus(first: list[int], second: list[int]) -> list[int]:
-    return [int(round(first[i] - second[i])) for i in range(len(first))]
-
-
-def multiply(first: list[int], second: list[int]) -> list[int]:
-    return [first[i] * second[i] for i in range(len(first))]
-
-
-def divide(first: list[int], second: list[int]) -> list[int]:
-    result = []
-    for i in range(len(first)):
-        if second[i] == 0:
-            print("Division by zero encountered.")
-            result.append(0)
+    def add(self, v):
+        if self.can_add(v):
+            self.coins += v
+            return True
         else:
-            result.append(int(round(first[i] / second[i])))
-    return result
+            return False
+# Вже після цього можна завершувати код, але я ще хочу дещо урізноманітнити його, тому
+    #ще написав такі функції (методи), як : 1) Отримати поточний баланс скарбнички
+    #                                       2) Вивести інформацію про скарбничку
+    #                                       3) Видати гроші з скарбнички, якщо можливо
+    def get_balance(self):
+        return self.coins
 
-FIRST_VECTOR = 'first_vector.txt'
-SECOND_VECTOR = 'second_vector.txt'
+    def print_info(self):
+        print(f"Місткість скарбнички: {self.capacity}")
+        print(f"Поточна кількість монет: {self.coins}")
 
-first_vector = retrieve_vector(FIRST_VECTOR)
-second_vector = retrieve_vector(SECOND_VECTOR)
+    def withdraw(self, v):
+        if self.coins >= v:
+            self.coins -= v
+            return True
+        else:
+            return False
 
-if first_vector and second_vector:
-    print("Multiplication:", multiply(first_vector, second_vector))
-    print("Addition:", plus(first_vector, second_vector))
-    print("Division:", divide(first_vector, second_vector))
-    print("Subtraction:", minus(first_vector, second_vector))
+n = int(input("Введіть місткість скарбнички: "))
+money_box = MoneyBox(n)
+m = int(input("Скільки монет поклали в скарбничку: "))
+money_box.coins = m
+k = int(input("Кількість монет, які хочуть покласти в скарбничку: "))
+if money_box.can_add(k):
+    result = money_box.add(k)
+    print(result)
+else:
+    print(False)
+money_box.print_info()
+withdraw_amount = int(input("Скільки монет ви хочете видати?: "))
+if money_box.withdraw(withdraw_amount):
+    print("Видано гроші.")
+else:
+    print("Недостатньо грошей для видання.")
+money_box.print_info()
