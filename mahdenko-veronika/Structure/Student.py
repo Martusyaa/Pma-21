e34aeb5b186ea6202c8d90bf3f9407ae5182f590
VeronikaMahdenko <nikamahdenko@gmail.com>
2023-11-25 16:30:32 +0200

    [mahdenko] added Rest

--- mahdenko-veronika/Structure/Student.py
+++ mahdenko-veronika/Structure/Student.py
@@ -1,3 +1,5 @@
+from pyxtension.streams import stream
+
 class Student:
     def __init__(self, name, surname, data_birthday, list_grade):
         self.name = name
@@ -5,9 +7,11 @@ class Student:
         self.data_birthday = data_birthday
         self.list_grade = list_grade
 
-    @staticmethod
-    def read_matrix_from_file(filename):
-        students = []
+class Dekanat:
+    def __init__(self):
+        self.students = []
+
+    def read_matrix_from_file(self, filename):
         try:
             with open(filename, 'r') as file:
                 for line in file:
@@ -19,7 +23,7 @@ class Student:
                         birth_date, *grades = parts[1].split(', ')
                         list_grade = [int(i) for i in str.split(*grades)]
                         student = Student(first_name, last_name, birth_date, list_grade)
-                        students.append(student)
+                        self.students.append(student)
                     except ValueError as e:
                         print(f"Неправильні дані: {e}")
                         continue
@@ -28,20 +32,11 @@ class Student:
         except Exception as e:
             print(f"Виникла помилка: {e}")
 
-        if not students:
+        if not self.students:
             print("Файл пустий.")
 
-        return students
-    # @staticmethod
-    # def print_failed_students(students):
-    #     for i in students.list_grade:
-    #         if i < 51:
-    #             print(f"{students.first_name} {students.last_name} не склав сесію")
-
-    @staticmethod
-    def print_failed_students(students):
-        for student in students:
-            for grade in student.list_grade:
-                failed = grade < 51
-                if failed:
-                    print(f"{student.name} {student.surname} не склав сесію")
+    def print_failed_students(self):
+        results = stream(self.students).flatMap(lambda student: [(student, grade) for grade in student.list_grade if grade < 51])
+        for result in results:
+            student, grade = result
+            print(f"{student.name} {student.surname} не склав сесію. Бал: {grade}")
