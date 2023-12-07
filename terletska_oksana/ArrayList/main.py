c5223249addeb988dbbb3474808c92becb66d4c2
OksanaTerletska <telezka.oksana@gmail.com>
2023-12-07 16:13:22 +0200

    TO: add homework

---
+++ terletska_oksana/ArrayList/main.py
@@ -0,0 +1,19 @@
+from Class import ArrayList
+
+my_list = ArrayList([1])
+print(my_list)
+
+my_list.add(6)
+print(my_list)
+
+my_list.remove(0)
+print(my_list)
+
+my_list.insert(10, 8)
+print(my_list)
+
+my_list.clear()
+print(my_list)
+
+my_list.remove(0)
+print(my_list)
